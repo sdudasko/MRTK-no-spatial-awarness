353944e5177be4ba82347806babd1144a2fc91a4
Stefan Dudasko <stefan.dudasko@gmail.com>
2022-03-13 18:43:04 +0100

    Added examples, select

---
+++ Assets/Scenes/SphereSc.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SphereSc : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // var colorTheme = this.GetComponent<Interactable>().ActiveThemes[0];
+
+        // colorTheme.StateProperties[0].Values[0].Color = Color.green;
+    }
+}
